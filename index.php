8f4aa80695a679e302f351702c9e266f113be6ab
V.Bunchuk <v.bunchuk@iotechnologies.com>
2019-01-04 14:57:15 +0200

    chart improvements

--- index.php
+++ index.php
@@ -2,84 +2,193 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>Зеленский - счетчик анкет</title>
     <style>
         body {
             background-color: #04133b;
+            font-family: 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif;
         }
 
         .chartBody {
             background-color: rgba(255, 255, 255, 0.03);
             padding: 25px;
         }
+
+        .center {
+            text-align: center
+        }
+
+        h5 {
+            margin: 5px;
+        }
+
+        p {
+            color: #fff;
+        }
     </style>
 </head>
 <body>
+<div class="center">
+    <h1 style="color: #fff;">Статистика анкет на сайте Ze2019.com</h1>
+    <h5 style="color: #ccc;">* график сгруппирован по 5 минут</h5>
+    <h5 style="color: #ccc;">* обновляется раз в минуту</h5>
+    <h5 style="color: #ccc;">* треккинг не работал с 9:00 до 10:35, поэтому наблюдается скачок</h5>
+</div>
+<div class="">
+    <p>Группировать по минутам</p>
+    <select name="" id="minutes">
+        <option value="1">1 минута</option>
+        <option selected value="5">5 минут</option>
+        <option value="15">15 минут</option>
+        <option value="60">1 час</option>
+    </select>
+    <input type="date" id="date" data-date="" data-date-format="DD MMMM YYYY">
+</div>
 <div class="chartBody">
-    <canvas id="myChart" style="height:50vh; width:80vw"></canvas>
+    <canvas id="myChart" style="height:40vh; width:80vw"></canvas>
 </div>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js"></script>
 
 <script>
-    var data;
+    var el = document.getElementById('minutes');
+    var minutes = el.value;
+    var dateEl = document.getElementById('date');
+    dateEl.valueAsDate = new Date();
+    var date = dateEl.value;
+    el.addEventListener('change', (e) => {
+        minutes = e.target.value;
+        load();
+    });
+    dateEl.addEventListener('change', (e) => {
+        date = e.target.value;
+        load();
+    });
+
+    var _toArray = function (arr) {
+        return Array.isArray(arr) ? arr : [].slice.call(arr);
+    };
+
+    var data = {
+        growth: {
+            data: [],
+            labels: []
+        },
+        vals: {
+            data: [],
+            labels: []
+        }
+    };
+
+    var options = {
+        tooltips: {
+            intersect: false,
+            mode: 'x'
+        },
+        animation: {
+            duration: 0
+        },
+        scales: {
+            yAxes: [
+                {
+                    id: 'y-axis-1',
+                    position: 'left',
+                    ticks: {
+                        beginAtZero: false,
+                        callback: function (value) {
+                            return Number(value.toString());
+                        }
+                    },
+                    type: 'logarithmic',
+                    gridLines: {
+                        color: 'rgba(41,169,255,0.2)'
+                    }
+                },
+                {
+                    id: 'y-axis-2',
+                    position: 'right',
+                    ticks: {
+                        beginAtZero: false,
+                        callback: function (value) {
+                            return Number(value.toString());
+                        }
+                    },
+                    type: 'logarithmic',
+                    gridLines: {
+                        color: 'rgba(41,169,255,0.2)'
+                    }
+                }
+            ],
+            xAxes: [{
+                display: false
+            }]
+        }
+    };
+
+    var ctx = document.getElementById("myChart").getContext('2d');
+
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: data.growth.labels,
+            datasets: [
+                {
+                    data: data.growth.data,
+                    borderWidth: 1.5,
+                    label: 'Прирост заполненных анкет',
+                    borderColor: '#2aabff',
+                    backgroundColor: 'rgba(41,169,255,0.3)',
+                    pointBackgroundColor: '#2aabff',
+                    pointRadius: 2,
+                    yAxisID: 'y-axis-1'
+                },
+                {
+                    data: data.vals.data,
+                    borderWidth: 1.5,
+                    label: 'Сумма заполненных анкет',
+                    borderColor: '#df6c5a',
+                    backgroundColor: 'rgba(223, 106, 88, 0.3)',
+                    pointBackgroundColor: '#df6c5a',
+                    pointRadius: 2,
+                    yAxisID: 'y-axis-2'
+                }]
+        },
+        options: options
+    });
+
     load();
 
     function load() {
-        fetch('http://127.0.0.1:8080/info.php')
-            .then((response) =>
-                response.json()
-            ).then((json) => {
-            data = json;
-            render();
-            setTimeout(() => {
-                load();
-            }, 30000);
-        });
+        fetch('/info.php?q=' + minutes + '&date=' + date)
+            .then((response) => response.json())
+            .then((json) => {
+                myChart.data.labels = json.growth.labels;
+                myChart.data.datasets[0].data = json.growth.data;
+                myChart.data.datasets[1].data = json.vals.data;
+
+                myChart.options.scales.yAxes[1].ticks.min = Math.min.apply(null, _toArray(json.vals.data));
+                myChart.options.scales.yAxes[1].ticks.max = Math.max.apply(null, _toArray(json.vals.data));
+                myChart.update();
+
+                setTimeout(load, 30000);
+            });
     }
-
-
-    function render() {
-        var ctx = document.getElementById("myChart").getContext('2d');
-        var myChart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: data.labels,
-                datasets: [{
-                    data: data.data,
-                    borderWidth: 1.5,
-                    label: 'Growth',
-                    borderColor: '#2aabff',
-                    backgroundColor: 'rgba(41,169,255,0.3)',
-                    pointBackgroundColor: '#2aabff'
-
-                }]
-            },
-            options: {
-                animation: {
-                    duration: 0
-                },
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true,
-                            callback: function (value) {
-                                return Number(value.toString());
-                            }
-                        },
-                        type: 'logarithmic',
-                        gridLines: {
-                            color: 'rgba(41,169,255,0.2)'
-                        }
-                    }],
-                    xAxes: [{
-                        display: false
-                    }]
-                }
-            }
-        });
-    }
-
 </script>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-129298968-2"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+
+    gtag('js', new Date());
+
+    gtag('config', 'UA-129298968-2');
+</script>
+
+
 </body>
 </html>
--- info.php
+++ info.php
@@ -6,23 +6,90 @@
  * Time: 18:25
  */
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "ze";
-$conn = new mysqli($servername, $username, $password, $dbname);
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET');
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 
-$sql = 'select * from counter';
-$res = $conn->query($sql);
+require_once __DIR__ . '/vendor/autoload.php';
 
-$rows = $res->fetch_all();
+
+$config = [
+    'host' => '127.0.0.1',
+    'port' => '8123',
+    'username' => 'default',
+    'password' => ''
+];
+
+$db = new ClickHouseDB\Client($config);
+$db->database('zes');
+
+//$sql = "select * from zes.counter3 WHERE growth != value";
+
+$availableQuantors = [
+    1,
+    5,
+    15,
+    60
+];
+
+$quantor = $_GET['q'] ?? 5;
+
+if (!in_array($quantor, $availableQuantors)) {
+    $quantor = 5;
+}
+
+$date = $_GET['date'] ?? (new \DateTimeImmutable())->format('Y-m-d');
+
+switch ($quantor) {
+    case 1:
+        $quantorFunction = 'time';
+        break;
+    case 5:
+        $quantorFunction = 'toStartOfFiveMinute(time)';
+        break;
+    case 15:
+        $quantorFunction = 'toStartOfFifteenMinutes(time)';
+        break;
+    case 60:
+        $quantorFunction = 'toStartOfHour(time)';
+        break;
+}
+
+$sql = "select sum(toInt16(growth)) as growth, {$quantorFunction} as time 
+ FROM zes.counter3
+ WHERE toDate(time) = '{$date}'
+ GROUP BY time 
+  order by time ASC";
+//print_r($sql); die;
+
+$res = $db->select($sql)->rows();
+
+
+$sql = "select MAX(value) as value, {$quantorFunction} as time
+ FROM zes.counter3
+ WHERE toDate(time) = '{$date}'
+ GROUP BY time
+order by time ASC";
+$res2 = $db->select($sql)->rows();
+
+$rows = $res;
 
 $array = [];
 foreach ($rows as $row) {
-    $array['labels'][] = $row[0];
-    $array['data'][] = (int)$row[2];
+    $array['labels'][] = date("Y-m-d H:i:s", strtotime($row['time'] . " + 2 hours"));
+    $array['data'][] = (int)$row['growth'];
 }
 
-print_r(json_encode($array));
+$array2 = [];
+foreach ($res2 as $row) {
+    $array2['labels'][] = date("Y-m-d H:i:s", strtotime($row['time'] . " + 2 hours"));
+    $array2['data'][] = (int)$row['value'];
+}
+
+$result = ['growth' => $array, 'vals' => $array2];
+echo json_encode($result);
 die;
