c978d6e77d09367d953a83d657eede8a8f393aae
kostya.adam <kostya.adam@onthe.io>
2019-01-04 13:29:13 +0200

    fix3

--- info.php
+++ info.php
@@ -13,9 +13,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-//include_once 'PDOlib/mysql.php';
 
-require_once dirname(__FILE__).'/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 
 $config = [
@@ -29,17 +28,47 @@ $db = new ClickHouseDB\Client($config);
 $db->database('zes');
 
 //$sql = "select * from zes.counter3 WHERE growth != value";
-$sql = "select sum(toInt16(growth)) as growth, toStartOfFiveMinute(time) as ttime 
+
+$availableQuantors = [
+    1,
+    5,
+    15,
+    60
+];
+
+$quantor = $_GET['q'] ?? 5;
+
+if (!in_array($quantor, $availableQuantors)) {
+    $quantor = 5;
+}
+
+switch ($quantor) {
+    case 1:
+        $quantorFunction = 'time';
+        break;
+    case 5:
+        $quantorFunction = 'toStartOfFiveMinute(time)';
+        break;
+    case 15:
+        $quantorFunction = 'toStartOfFifteenMinutes(time)';
+        break;
+    case 60:
+        $quantorFunction = 'toStartOfHour(time)';
+        break;
+}
+
+$sql = "select sum(toInt16(growth)) as growth, {$quantorFunction} as time 
  FROM zes.counter3
- GROUP BY ttime 
-  HAVING (growth != -18908)
-order by ttime ASC";
+ WHERE date >= '2019-01-04'
+ GROUP BY time 
+  order by time ASC";
 
 $res = $db->select($sql)->rows();
 
 
-$sql = "select MAX(value) as value, toStartOfFiveMinute(time) as time
+$sql = "select MAX(value) as value, {$quantorFunction} as time
  FROM zes.counter3
+ WHERE date >= '2019-01-04'
  GROUP BY time
 order by time ASC";
 $res2 = $db->select($sql)->rows();
@@ -48,16 +77,16 @@ $rows = $res;
 
 $array = [];
 foreach ($rows as $row) {
-    $array['labels'][] = date("Y-m-d H:i:s", strtotime($row['ttime']." + 2 hours"));
+    $array['labels'][] = date("Y-m-d H:i:s", strtotime($row['time'] . " + 2 hours"));
     $array['data'][] = (int)$row['growth'];
 }
 
 $array2 = [];
 foreach ($res2 as $row) {
-    $array2['labels'][] = date("Y-m-d H:i:s", strtotime($row['time']." + 2 hours"));
+    $array2['labels'][] = date("Y-m-d H:i:s", strtotime($row['time'] . " + 2 hours"));
     $array2['data'][] = (int)$row['value'];
 }
 
-$result  = ['growth' => $array, 'vals' => $array2];
-print_r(json_encode($result));
+$result = ['growth' => $array, 'vals' => $array2];
+echo json_encode($result);
 die;
