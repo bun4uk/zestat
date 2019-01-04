f82e63586388687a1c83e87dd8fbbfa4426945a8
kostya.adam <kostya.adam@onthe.io>
2019-01-04 11:32:46 +0200

    fix2

--- index.php
+++ index.php
@@ -6,81 +6,121 @@
     <style>
         body {
             background-color: #04133b;
+            font-family: sans-serif;
         }
 
         .chartBody {
             background-color: rgba(255, 255, 255, 0.03);
             padding: 25px;
         }
+
+        .center {
+            text-align: center
+        }
     </style>
 </head>
 <body>
-<div style="text-align: center">
+<div class="center">
     <h1 style="color: #fff;">Статистика анкет на сайте Ze2019.com</h1>
     <h4 style="color: #ccc;">* обновляется раз в минуту</h4>
 </div>
 <div class="chartBody">
     <canvas id="myChart" style="height:50vh; width:80vw"></canvas>
+    <canvas id="myChart2" style="height:50vh; width:80vw"></canvas>
 </div>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js"></script>
 
 <script>
-    var data;
+    var data = {
+        growth: {
+            data: [],
+            labels: []
+        },
+        vals: {
+            data: [],
+            labels: []
+        }
+    };
+
+    var options = {
+        animation: {
+            duration: 0
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true,
+                    callback: function (value) {
+                        return Number(value.toString());
+                    }
+                },
+                type: 'logarithmic',
+                gridLines: {
+                    color: 'rgba(41,169,255,0.2)'
+                }
+            }],
+            xAxes: [{
+                display: false
+            }]
+        }
+    }
+
+    var ctx = document.getElementById("myChart").getContext('2d');
+    var ctx2 = document.getElementById("myChart2").getContext('2d');
+
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: data.growth.labels,
+            datasets: [{
+                data: data.growth.data,
+                borderWidth: 1.5,
+                label: 'Growth',
+                borderColor: '#2aabff',
+                backgroundColor: 'rgba(41,169,255,0.3)',
+                pointBackgroundColor: '#2aabff',
+                pointRadius: 2
+
+            }]
+        },
+        options: options
+    });
+
+    var myChart2 = new Chart(ctx2, {
+        type: 'line',
+        data: {
+            labels: data.vals.labels,
+            datasets: [{
+                data: data.vals.data,
+                borderWidth: 1.5,
+                label: 'Growth',
+                borderColor: '#2aabff',
+                backgroundColor: 'rgba(41,169,255,0.3)',
+                pointBackgroundColor: '#2aabff',
+                pointRadius: 2
+
+            }]
+        },
+        options: options
+    });
+
     load();
 
     function load() {
         fetch('/info.php')
             .then((response) => response.json())
             .then((json) => {
-                data = json;
-                render();
+                myChart.data.labels = json.growth.labels;
+                myChart.data.datasets[0].data = json.growth.data;
+                myChart.update();
+
+                myChart2.data.labels = json.vals.labels;
+                myChart2.data.datasets[0].data = json.vals.data;
+                myChart2.update();
 
                 setTimeout(load, 30000);
             });
-    }
-
-
-    function render() {
-        var ctx = document.getElementById("myChart").getContext('2d');
-        var myChart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: data.labels,
-                datasets: [{
-                    data: data.data,
-                    borderWidth: 1.5,
-                    label: 'Growth',
-                    borderColor: '#2aabff',
-                    backgroundColor: 'rgba(41,169,255,0.3)',
-                    pointBackgroundColor: '#2aabff',
-                    pointRadius: 2
-
-                }]
-            },
-            options: {
-                animation: {
-                    duration: 0
-                },
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true,
-                            callback: function (value) {
-                                return Number(value.toString());
-                            }
-                        },
-                        type: 'logarithmic',
-                        gridLines: {
-                            color: 'rgba(41,169,255,0.2)'
-                        }
-                    }],
-                    xAxes: [{
-                        display: false
-                    }]
-                }
-            }
-        });
     }
 </script>
 
--- info.php
+++ info.php
@@ -6,6 +6,9 @@
  * Time: 18:25
  */
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET');
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -25,18 +28,36 @@ $config = [
 $db = new ClickHouseDB\Client($config);
 $db->database('zes');
 
-$sql = "select * from zes.counter3 WHERE growth != value";
+//$sql = "select * from zes.counter3 WHERE growth != value";
+$sql = "select sum(toInt16(growth)) as growth, toStartOfFiveMinute(time) as ttime 
+ FROM zes.counter3
+ GROUP BY ttime 
+  HAVING (growth != -18908)
+order by ttime ASC";
+
 $res = $db->select($sql)->rows();
 
-//print_r($res);
+
+$sql = "select MAX(value) as value, toStartOfFiveMinute(time) as time
+ FROM zes.counter3
+ GROUP BY time
+order by time ASC";
+$res2 = $db->select($sql)->rows();
 
 $rows = $res;
 
 $array = [];
 foreach ($rows as $row) {
-    $array['labels'][] = $row['time'];
+    $array['labels'][] = date("Y-m-d H:i:s", strtotime($row['ttime']." + 2 hours"));
     $array['data'][] = (int)$row['growth'];
 }
 
-print_r(json_encode($array));
+$array2 = [];
+foreach ($res2 as $row) {
+    $array2['labels'][] = date("Y-m-d H:i:s", strtotime($row['time']." + 2 hours"));
+    $array2['data'][] = (int)$row['value'];
+}
+
+$result  = ['growth' => $array, 'vals' => $array2];
+print_r(json_encode($result));
 die;
